03e0466bfa2ec8d8e7f1d7bf288101c7897d96e3
dc91_ <dc91_@hotmail.com>
2025-01-20 20:44:59 +0100

    Search/filter update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   adapter: node({
     // mode: 'standalone' or 'serverless'
     mode: 'standalone',
