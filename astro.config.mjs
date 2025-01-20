847781dac9a79ea04ab0f65a734887c3d6642dbb
Pierre Chateau <pierre@chateau.se>
2025-01-20 23:45:34 +0100

    new commit uses api to fetch data. you need to create a file called .env in root directory, check teams for more info!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  env: {
+    schema: {
+      API_SECRET_TOKEN: envField.string({ context: "server", access: "secret" }),
+    },
+  },
   adapter: node({
     // mode: 'standalone' or 'serverless'
-    mode: 'standalone',
+    mode: "standalone",
   }),
   integrations: [react(), tailwind()],
 });
