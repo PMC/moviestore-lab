ec53835c79a378b50db4b00450b9a265e7538326
Pierre Chateau <pierre@chateau.se>
2025-01-21 09:55:27 +0100

    removed old code

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,10 +5,6 @@ const apiMovies = defineCollection({
     loader: file("src/content/movies.json", { parser: (text) => JSON.parse(text).results })
 });
 
-const movieInfo = defineCollection({
-    loader: file("src/content/939243.json", { parser: (text) => JSON.parse(text).results })
-});
-
 const movieCategories = defineCollection({
     loader: file("src/content/categories.json", { parser: (text) => JSON.parse(text).genres })
 });
@@ -17,5 +13,5 @@ const movieCategories = defineCollection({
 
 
  export const collections = {
-     apiMovies, movieInfo, movieCategories
+     apiMovies, movieCategories
  };
