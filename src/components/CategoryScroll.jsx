9599301fb59e56b2b877226939f7c397bf117a75
Pierre Chateau <pierre@chateau.se>
2025-01-22 16:17:06 +0100

    fix for class=

--- src/components/CategoryScroll.jsx
+++ src/components/CategoryScroll.jsx
@@ -4,9 +4,7 @@ import { createMoviePrice } from "../scripts/moviePrices";
 const movies = moviesData.results;
 
 function CategoryScroll({ category, movies }) {
-  const categoryMovies = movies
-    .filter((m) => m.genre_ids.includes(category.id))
-    .slice(0, 20);
+  const categoryMovies = movies.filter((m) => m.genre_ids.includes(category.id)).slice(0, 20);
 
   return (
     <section className="category-scroll-section">
@@ -18,19 +16,12 @@ function CategoryScroll({ category, movies }) {
         {categoryMovies.map((movie) => (
           <article className="card movie-poster" key={movie.id}>
             <a href={`./movie/info/${movie.id}`}>
-              <img
-                src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                alt={movie.title}
-              />
+              <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
               <h4 className="movieTitle">{movie.title}</h4>
             </a>
-            
+
             <div className="rating">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 115.53 110.12"
-                className="star-icon"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115.53 110.12" className="star-icon">
                 <polygon
                   className="starSvg"
                   points="57.77 2.5 74.84 37.1 113.03 42.65 85.4 69.59 91.92 107.62 57.77 89.66 23.61 107.62 30.13 69.59 2.5 42.65 40.69 37.1 57.77 2.5"
@@ -38,8 +29,11 @@ function CategoryScroll({ category, movies }) {
               </svg>
               <span className="star-rating">{movie.vote_average.toFixed(1)}</span>
             </div>
-            <span class="unit-price">{createMoviePrice(movie.vote_average)}</span>
-            <button className="add-to-cart" data-movieid={movie.id}> Add to cart </button>
+            <span className="unit-price">{createMoviePrice(movie.vote_average)}</span>
+            <button className="add-to-cart" data-movieid={movie.id}>
+              {" "}
+              Add to cart{" "}
+            </button>
           </article>
         ))}
       </div>
